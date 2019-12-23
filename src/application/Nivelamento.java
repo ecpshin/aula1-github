557e37eb5a0aa85d2d051a9785bb6cf2e02aeb43
ecpshin <francisco.shin@gmail.com>
2019-12-23 10:46:47 -0300

    Criacao da Classe Nivelemento

--- src/application/Nivelamento.java
+++ src/application/Nivelamento.java
@@ -1,9 +1,17 @@
 package application;
 
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
+
 public class Nivelamento {
 	
 	public static void main(String[] args) {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		
+		Date d = Date.from(Instant.parse("2019-12-23T10:42:00Z"));
+		
+		System.out.println(sdf.format(d));
 	}
 }
